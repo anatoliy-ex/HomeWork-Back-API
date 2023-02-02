ca81f2cfbef6423a35cd476127b170dd36b63dc8
xx-Flex-xx <endlessxxxpain@gmail.com>
2023-02-03 01:04:07 +0300

    Add hello

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import express,{Request, Response} from 'express'
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 const parserMiddleware = express.json()
 app.use(parserMiddleware)
 
 app.get('/', (req: Request, res: Response) => {
-    let helloWorld = 'Hello66PP!!!';
+    let helloWorld = 'Hello!';
     res.send(helloWorld)
 })
 
