52dbc009d246e2b3388373dc37e673085d6afe84
xx-Flex-xx <endlessxxxpain@gmail.com>
2023-02-03 00:04:39 +0300

    Parser

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import express,{Request, Response} from 'express'
 const app = express()
 const port = 3000
 
+const parserMiddleware = express.json()
+app.use(parserMiddleware)
+
 app.get('/', (req: Request, res: Response) => {
     let helloWorld = 'Hello World!!!PPPP66666PP!!!';
     res.send(helloWorld)
