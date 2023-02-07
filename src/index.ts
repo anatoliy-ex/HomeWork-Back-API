327a42da50e4d0d3da7a05e5afaa3ad22db98869
xx-Flex-xx <endlessxxxpain@gmail.com>
2023-02-07 21:16:23 +0300

    Add routers v2

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express, {Request,Response} from 'express'
 import {hwVideosRouter} from "./routes/hw-videos-router";
 import {hwTestingRouter} from "./routes/hw-testing-router";
+import bodyParser from "body-parser";
 
 const app = express()
 const port = 3000
+const parserMiddleware = bodyParser({})
+app.use(parserMiddleware)
 
 app.use(express.json)
 
